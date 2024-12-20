53f9905e22ed2f7f50722120a7d211b240bd270c
Ashish Kumar <ashish380431@gmail.com>
2024-12-20 13:20:09 +0530

    bug fixes

--- src/app/components/posts/PostListing.tsx
+++ src/app/components/posts/PostListing.tsx
@@ -3,15 +3,21 @@
 import React from 'react';
 import PostCard from './PostCard';
 import HeaderFooterLayout from '../layout/HeaderFooterLayout';
+import Link from 'next/link';
 
 export const PostListing: React.FC<{ posts: IPost[] }> = ({ posts }) => {
   return (
     <HeaderFooterLayout isAuthPage={true}>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {posts.map((post) => (
-        <PostCard key={post.id} id={post.id} title={post.title} body={post.body} />
-      ))}
-    </div>
+      <div className="flex justify-center items-center">
+        <Link href="/posts/add-update">
+          <div className="text-red-500 font-bold cursor-pointer">Add Post</div>
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {posts.map((post) => (
+          <PostCard key={post.id} id={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
     </HeaderFooterLayout>
   );
 };
